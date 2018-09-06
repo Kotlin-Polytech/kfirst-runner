294d9aa4961886e9f43fcc329e4159f8f53fb4c9
ice-phoenix <Marat.Akhin@gmail.com>
2018-09-06 16:16:19 +0300

    0.1.17 release (better maven integration)

--- src/main/kotlin/org/jetbrains/research/runner/util/MavenUtils.kt
+++ src/main/kotlin/org/jetbrains/research/runner/util/MavenUtils.kt
@@ -2,46 +2,12 @@ package org.jetbrains.research.runner.util
 
 import org.apache.maven.model.Dependency
 import org.apache.maven.model.Model
-import org.apache.maven.model.building.DefaultModelBuilderFactory
-import org.apache.maven.model.building.DefaultModelBuildingRequest
-import org.apache.maven.model.interpolation.StringSearchModelInterpolator
-import org.apache.maven.repository.internal.MavenRepositorySystemUtils
 import org.eclipse.aether.RepositorySystem
 import org.eclipse.aether.RepositorySystemSession
 import org.eclipse.aether.collection.CollectRequest
-import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory
-import org.eclipse.aether.repository.LocalRepository
 import org.eclipse.aether.repository.RemoteRepository
 import org.eclipse.aether.resolution.ArtifactResult
 import org.eclipse.aether.resolution.DependencyRequest
-import org.eclipse.aether.spi.connector.RepositoryConnectorFactory
-import org.eclipse.aether.spi.connector.transport.TransporterFactory
-import org.eclipse.aether.transport.file.FileTransporterFactory
-import org.eclipse.aether.transport.http.HttpTransporterFactory
-import java.io.File
-
-internal fun newRepositorySystem(): RepositorySystem {
-    return MavenRepositorySystemUtils.newServiceLocator()
-            .addService(RepositoryConnectorFactory::class.java, BasicRepositoryConnectorFactory::class.java)
-            .addService(TransporterFactory::class.java, FileTransporterFactory::class.java)
-            .addService(TransporterFactory::class.java, HttpTransporterFactory::class.java)
-            .getService(RepositorySystem::class.java)
-}
-
-internal fun newSession(system: RepositorySystem): RepositorySystemSession {
-    val session = MavenRepositorySystemUtils.newSession()
-    val localRepo = LocalRepository(System.getProperty("user.home") + "/.m2/repository")
-    session.localRepositoryManager = system.newLocalRepositoryManager(session, localRepo)
-    return session
-}
-
-internal fun localRepo(): RemoteRepository {
-    return RemoteRepository.Builder(
-            "local",
-            "default",
-            "file:" + System.getProperty("user.home") + "/.m2/repository"
-    ).build()
-}
 
 internal val Dependency.asAether: org.eclipse.aether.graph.Dependency
     get() = org.eclipse.aether.graph.Dependency(
@@ -55,29 +21,19 @@ internal val Dependency.asAether: org.eclipse.aether.graph.Dependency
             this.isOptional
     )
 
-fun getPomModel(pom: File): Model {
-    val pomReq = DefaultModelBuildingRequest().setPomFile(pom)
-    val b = DefaultModelBuilderFactory().newInstance()
-    return StringSearchModelInterpolator().interpolateModel(
-            b.build(pomReq).rawModel,
-            pom.parentFile,
-            pomReq,
-            {}
-    )
-}
-
-fun getClasspathFromModel(model: Model): List<ArtifactResult> {
+fun getClasspathFromModel(
+        model: Model,
+        repoSystem: RepositorySystem,
+        session: RepositorySystemSession,
+        repos: List<RemoteRepository>
+): List<ArtifactResult> {
 
     // TODO: get Kotlin roots from Maven info
-
-    val repoSystem = newRepositorySystem()
-    val session = newSession(repoSystem)
-    val localRepo = localRepo()
 
     val collectReq = CollectRequest(
             model.dependencies.map { it.asAether },
             null,
-            listOf(localRepo)
+            repos
     )
     val depReq = DependencyRequest(collectReq, null)
 
@@ -86,6 +42,3 @@ fun getClasspathFromModel(model: Model): List<ArtifactResult> {
     return res.artifactResults
             .filter { it.isResolved }
 }
-
-fun getClasspathFromPom(pom: File): List<ArtifactResult> =
-        getClasspathFromModel(getPomModel(pom))
