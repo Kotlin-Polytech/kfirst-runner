2f1ceceb62f734ddd2882249345e047895cac80e
vitekkor <viktor.korotkikh@gmail.com>
2021-05-29 00:15:38 +0300

    ZestTest support

--- src/main/kotlin/org/jetbrains/research/runner/util/Util.kt
+++ src/main/kotlin/org/jetbrains/research/runner/util/Util.kt
@@ -1,9 +1,22 @@
 package org.jetbrains.research.runner.util
 
 fun String.mapLines(f: (String) -> String): String =
-        lineSequence().map(f).joinToString("\n")
+    lineSequence().map(f).joinToString("\n")
 
 fun codifyString(any: Any?, indent: String): String {
     val result = "$any"
     return "```\n$result\n```".mapLines { "    " + indent + it }
+}
+
+fun loadZestClasses(packages: List<String>): Array<Class<*>> {
+    val result = ArrayList<Class<*>>()
+    for (pkg in packages) {
+        try {
+            val zestCass = Thread.currentThread().contextClassLoader.loadClass("$pkg.ZestTests")
+            result.add(zestCass)
+        } catch (ex: ClassNotFoundException) {
+
+        }
+    }
+    return result.toTypedArray()
 }
